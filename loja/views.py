4fd46debd3df2f694b249aea56e09ccb04dfbe76
Jonas <ferreirajonasss@gmail.com>
2018-08-09 20:55:55 -0300

    adicionados serializers e views basicas de produtos e pedidos

--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from loja.models import Produto
+from loja.models import Pedido
+from loja.serializers import ProdutoSerializer
+from loja.serializers import PedidoSerializer
 
-# Create your views here.
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
