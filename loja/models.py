36886ce381ec39e9102b5ea89b6937ea5b4cd608
Jonas <ferreirajonasss@gmail.com>
2018-08-09 17:29:49 -0300

    modelagem inicial

--- loja/models.py
+++ loja/models.py
@@ -11,22 +11,6 @@ CATEGORIAS = (
     ( 7, 'Fonte'),
 )
 
-
-class Produto(models.Model):
-    
-    # categoria
-    # nome
-    # descricao
-    # valor
-    pass
-
-
-class Estoque(models.Model):
-
-    # produto = 
-    # quantidade =     
-
-
 STATUS_PEDIDO = (
     (1,'Pedido Realizado'),
     (2,'Separação em estoque'),
@@ -34,8 +18,24 @@ STATUS_PEDIDO = (
     (4,'Realização de testes'),
     (5,'Concluído'),
 )
+
+
+class Produto(models.Model):
+    
+    categoria = models.PositiveIntegerField(choices=CATEGORIAS, default=1)
+    nome = models.CharField(max_length=40)
+    descricao = models.TextField()
+    valor = models.DecimalField()
+
+
 class Pedido(models.Model):
     
-    # produtos = 
-    # status = 
-    pass
+    produtos = models.ManyToManyField(Produto)
+    status = models.PositiveIntegerField(choices=STATUS_PEDIDO)
+    data_realizacao = models.DateTimeField(auto_now_add=True)
+
+
+class ProdutoEstoque(models.Model):
+
+    produto = models.OneToOneField(Produto)
+    quantidade = models.PositiveIntegerField(default=1)    
