107ef773236ca6b3f591a899ebd75a56e39f6f8f
Jonas <ferreirajonasss@gmail.com>
2018-08-13 08:58:54 -0300

    liberando view de webhook para requisições

--- trello_app/views.py
+++ trello_app/views.py
@@ -3,10 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
 
 
 
 class WebHookTrelloView(APIView):
+    
+    permission_classes = (AllowAny,)
     
     def get(self, request, format=None):
         """
